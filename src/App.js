35c7b627083f54ec1c56ad27d06d642ade43ccac
poojachaturvedi <poojachaturvedi1672002@gmail.com>
2023-01-30 19:14:59 +0530

    changes made

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+// import UseCounter from './custom-hooks/UseCounter'
+import { useState } from "react";
+import useList from './custom-hooks/useList';
+import styles from './App.module.css'
 
-function App() {
+// import Like from './Components/Like';
+
+export default function App() {
+  // const {count,increment,decrement}=UseCounter()
+  const {list,push,pull}=useList()
+  const[todo,settodo]=useState("")
+  const onsubmithandler=(event)=>{
+    event.preventDefault();
+    push(todo)
+    settodo("")
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className={styles.body}>
+      {/* {count}
+      <button onClick={increment}>Add</button>
+      <button onClick={decrement}>Subtract</button>
+      <Like/> */}
+      <div>
+        <h1 className={styles.heading}>To DO LIST</h1>
+        <header>
+          <form onSubmit={(event)=>{onsubmithandler(event)}}>
+            <label>Add Todo Item</label>
+            <input type="text" onChange={(e)=>{settodo(e.target.value)}} value={todo} />
+            <button type="submit">Submit</button>
+          </form>
+          <h5 className={styles.task}>
+          {
+          list.map((listItem,listIndex)=>{
+            return(<>
+            <li key={listIndex}>{listItem}</li>
+            <button onClick={()=>{pull(listIndex)}}>Remove Index</button>
+            </>
+            ) 
 
-export default App;
+          })}
+          </h5>
+        </header>
+      </div>
+    </div>
+
+  )
+}
